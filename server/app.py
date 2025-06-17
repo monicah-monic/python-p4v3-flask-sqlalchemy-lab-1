f2d89a28b0485155272b7cf91af67802aac30c41
monicah-monic <monicahwanjiru205@gmail.com>
2025-06-17 16:24:19 +0300

    sqlalchemy falsk

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,7 +20,30 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def get_eartquake_by_id(id):
+        earthquake = Earthquake.query.get(id)
+        if earthquake:
+            return make_response(jsonify(earthquake.to_dict(),201))
+        else:
+            return make_response(jsonify({"message": "Earthquake 9999 not found."}),404)
+        
+@app.route('/earthquakes/magnitude/<float:magnitude>') 
+def get_all(mag):
+     earthquakes = Earthquake.query.filter(Earthquake.magnitude >= mag).all()   
+     if earthquakes:
+          quakes_list = [quake.to_dict() for quake in earthquakes]
+          return jsonify({
+               "count": len(quakes_list),
+                "quakes": quakes_list
+          }),200
+     else:
+        return jsonify({
+            "count": 0,
+            "quakes": []
+        }), 200
+
+       
 
 
 if __name__ == '__main__':
